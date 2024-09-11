607cfff32d953822d8acb7f50d2233c8c1cc9d2c
naruemon2546z@gmail.com <naruemon2546z@gmail.com>
2024-09-11 17:51:19 +0700

    push

---
+++ myapp/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./templates/**/*.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        Honk: ["Honk"],
+        IBMPlexSansThai:["IBM Plex Sans Thai"],
+      }
+    },
+  },
+  plugins: [],
+}
+
